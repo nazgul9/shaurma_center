0881f17dd249cfb04649b20ebc939270277f9d82
nazgul9 <nazgul.abdibalieva.kk@gmail.com>
2022-04-14 14:18:21 +0600

    slider

--- src/component/g/Menu.js
+++ src/component/g/Menu.js
@@ -8,35 +8,37 @@ function Menu(props) {
         <div className=' m-2 p-2'>
             <h1 className='inline-block m-2 text-center pricePizza'>МЕНЮ:</h1>
 
+
+            <Card className=' m-2 texth1' as={Link} to="/Zavtract" style={{ borderRadius: "50px" }}><h1 >Завтрак</h1></Card>
+
             <Card className=' m-2 texth1' as={Link} to='/Burger' style={{ borderRadius: "50px" }}><h1 >Бургеры</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to="/Desert" style={{ borderRadius: "50px" }}><h1 >Десерты</h1></Card>
+            <Card className=' m-2 texth1' as={Link} to="/Shaurma" style={{ borderRadius: "50px" }}><h1 >Шаурма</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to="/Napitki" style={{ borderRadius: "50px" }}><h1 >Напитки</h1></Card>
+            <Card className=' m-2 texth1' as={Link} to="/Pizza" style={{ borderRadius: "50px" }}><h1 >Пицца</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to="/GNapit" style={{ borderRadius: "50px" }}><h1 >Горячии Напитки</h1></Card>
+            <Card className=' m-2 texth1' as={Link} to='/Krylyshki' style={{ borderRadius: "50px" }}><h1>Крылышки</h1></Card>
 
             <Card className=' m-2 texth1' as={Link} to="/GRool" style={{ borderRadius: "50px" }}><h1 >Горячий Роллы</h1></Card> 
 
              <Card className=' m-2 texth1' as={Link} to="/HRool" style={{ borderRadius: "50px" }}><h1 >Холодные Роллы</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to='/Krylyshki' style={{ borderRadius: "50px" }}><h1>Крылышки</h1></Card>
+            <Card className=' m-2 texth1' as={Link} to="/GNapit" style={{ borderRadius: "50px" }}><h1 >Горячии Напитки</h1></Card>
+            
+            <Card className=' m-2 texth1' as={Link} to="/Napitki" style={{ borderRadius: "50px" }}><h1 >Напитки</h1></Card>
+
+            <Card className=' m-2 texth1' as={Link} to="/Soci" style={{ borderRadius: "50px" }}><h1 >Сок</h1></Card>
 
             <Card className=' m-2 texth1' as={Link} to="/NasiBlud" style={{ borderRadius: "50px" }}><h1 >Блюда</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to="/Pizza" style={{ borderRadius: "50px" }}><h1 >Пицца</h1></Card>
-
             <Card className=' m-2 texth1' as={Link} to="/Salad" style={{ borderRadius: "50px" }}><h1 >Салат</h1></Card>
-
-            <Card className=' m-2 texth1' as={Link} to="/Shaurma" style={{ borderRadius: "50px" }}><h1 >Шаурма</h1></Card>
-
-            <Card className=' m-2 texth1' as={Link} to="/Soci" style={{ borderRadius: "50px" }}><h1 >Сок</h1></Card>
+           
+            <Card className=' m-2 texth1' as={Link} to="/Desert" style={{ borderRadius: "50px" }}><h1 >Десерты</h1></Card>
 
             <Card className=' m-2 texth1' as={Link} to="/Sous" style={{ borderRadius: "50px" }}><h1 >Соус</h1></Card>
 
             <Card className=' m-2 texth1' as={Link} to="/Wok" style={{ borderRadius: "50px" }}><h1 >Вок</h1></Card>
 
-            <Card className=' m-2 texth1' as={Link} to="/Zavtract" style={{ borderRadius: "50px" }}><h1 >Завтрак</h1></Card>
 
         </div>
     );
--- src/component/g/Slider.js
+++ src/component/g/Slider.js
@@ -1,5 +1,9 @@
 import {Carousel,  } from 'react-bootstrap'
 import {useState, render } from 'react'
+import b from './img/b.jpg'
+import bb from './img/bb.jpg'
+
+
 function Slider() {
     const [index, setIndex] = useState(0);
   
@@ -26,7 +30,7 @@ function Slider() {
             src="https://dinein.me/assets/dine-in-exceptional-dining.jpg"
             alt="Second slide"
           />
-  
+         
           <Carousel.Caption>
             {/* <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
@@ -36,6 +40,28 @@ function Slider() {
           <img 
             className="d-block w-100 c1"
             src="https://www.hoteloleana.com/wp-content/uploads/2018/02/stay-dine-01.jpg"
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3> */}
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img 
+            className="d-block w-100 c1"
+            src={b}
+            alt="besoft"
+          />
+  
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3> */}
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img 
+            className="d-block w-100 c1"
+            src={bb}
             alt="Third slide"
           />
   
